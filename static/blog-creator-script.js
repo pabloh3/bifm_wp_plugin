edfe765add9f3b77ec7f4cd2212080e8d01101c3
pabloh3 <pablo.hdz.sanz@gmail.com>
2023-12-04 16:58:43 -0800

    catch no password

--- static/blog-creator-script.js
+++ static/blog-creator-script.js
@@ -70,8 +70,8 @@ jQuery(document).ready(function($) {
                 pollForResults(jobId);
             } else {
                 console.log(response.data);
-                if (JSON.parse(response.data).message){
-                    $('#cbc_response').html(JSON.parse(response.data).message);
+                if (response.data.message){
+                    $('#cbc_response').html(response.data.message);
                 }
                 else{
                     $('#cbc_response').html(response.message ? response.message : 'An unknown error occurred.');
