f994779829312597bf10e16f4dba8e41da8f78c0
pabloh3 <pablo.hdz.sanz@gmail.com>
2024-05-06 15:26:24 -0700

    V14 corrected

--- widget-manager.php
+++ widget-manager.php
@@ -2,13 +2,13 @@
 /*
 Plugin Name: Build It For Me - AI creator
 Description: Ask a bot to create for you.
-Version: 1.0.13
+Version: 1.0.14
 Author: Build It For Me
 */
 // include the WordPress HTTP API
 include_once(ABSPATH . WPINC . '/http.php');
 require 'bifm-config.php';
-$current_version = '1.0.13';
+$current_version = '1.0.14';
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
